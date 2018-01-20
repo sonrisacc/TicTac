cdbb59c47dd905bed7b0ff3049538aebf8347a92
Sonrisa <sonrisa722611@gmail.com>
2018-01-19 19:43:46 -0800

    Update basic template

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import Board from './Board';
 
 class Game extends Component {
   render() {
-    return <div>I am properly rendered</div>;
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
   }
 }
 export default Game;
